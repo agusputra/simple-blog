7acb0e0429f5b656636aee4959967121d7af34b8
Agus Syahputra <agus.putra99@gmail.com>
2021-03-03 10:17:37 +0700

    add policies

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Post;
+use App\User;
+use App\Enums\ROLE_TYPE;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        return ($user->id === $post->user_id) ||
+            ($user->hasRole(ROLE_TYPE::manager)) ||
+            ($user->hasRole(ROLE_TYPE::admin));
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        return ($user->id === $post->user_id) ||
+            ($user->hasRole(ROLE_TYPE::manager)) ||
+            ($user->hasRole(ROLE_TYPE::admin));
+    }
+}
